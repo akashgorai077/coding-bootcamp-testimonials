914da52fec6d13b5c26d102b2970a8e30ce266b9
akashgorai077 <akashgorai077@gmailcom>
2024-09-11 12:54:57 +0530

    some change

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -22,7 +22,7 @@ function Slider() {
       text: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
       name: 'Tanya Sinclair',
       title: 'UX Engineer',
-      image: Tanya,
+      image: Tanya, 
     },
     {
       text: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
